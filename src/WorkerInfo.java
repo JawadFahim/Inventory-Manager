9480e009af303ce2bc41b7c289221ba36189c969
Jawad Fahim <72640742+JawadFahim@users.noreply.github.com>
2023-05-02 10:52:34 +0600

    Update WorkerInfo.java

--- src/WorkerInfo.java
+++ src/WorkerInfo.java
@@ -8,23 +8,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class WorkerInfo {
+public class WorkerInfo implements DemoWork {
     Scanner input = new Scanner(System.in);
     Worker theWorkers[] = new Worker[50];
     private String filePath="C:\\Users\\suve5\\Downloads\\StockPro Demo\\worker.txt";
     public static int count = 0;
 
     public void addWorker(Worker s) {
-        for (int i = 0; i < count; i++) {
+      /* for (int i = 0; i < count; i++) {
             if (s.regNum.equalsIgnoreCase(theWorkers[i].regNum)) {
                 // Print statement
                 System.out.println("Worker of Reg Num " + s.regNum + " is Already Registered.");
                 return;
             }
-        }
+        }*/
 
         if (count < 50) {
-            // String filePath = "C:\\Users\\suve5\\Downloads\\StockPro Demo\\worker.txt";
             try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath, true))) {
                 bw.write(s.regNum+","+s.workerName);
                 bw.newLine();
@@ -64,9 +63,8 @@ public class WorkerInfo {
 
 
     public int isWorker() {
-        System.out.println("Enter Reg Number:");
+        System.out.println("Enter your registration number:");
         String regNum = input.nextLine();
-        boolean G=true;
 
         try {
             // Read data from file
@@ -97,18 +95,18 @@ public class WorkerInfo {
     }
 
 
-
-    public void checkOutPro() {
-
+    public void checkInPro() {
+        int index = isWorker();
+        if (index != -1) {
             Scanner input = new Scanner(System.in);
             System.out.println("Enter registration number of the item to update:");
             String regNum = input.nextLine();
-
+    
             try {
                 // Open the file for reading
-                File file = new File("C:\\Users\\suve5\\Downloads\\StockPro Demo\\inventory.txt");
+                File file = new File( "C:\\Users\\suve5\\Downloads\\StockPro Demo\\inventory.txt");
                 Scanner scanner = new Scanner(file);
-
+    
                 // Create an ArrayList to store the items
                 ArrayList<String[]> items = new ArrayList<>();
 
@@ -119,39 +117,109 @@ public class WorkerInfo {
                     items.add(tokens);
                 }
                 scanner.close();
-
+    
                 // Find the item with the matching registration number and update the quantity
                 boolean found = false;
                 for (String[] item : items) {
                     if (item[0].equals(regNum)) {
                         found = true;
-                        System.out.println("Enter new quantity for " + item[1] + ":");
-                        int newQty = input.nextInt();
-                        item[2] = Integer.toString(newQty);
+                        System.out.println("Enter quantity to check in for " + item[1] + ":");
+                        String qtyStr = input.nextLine();
+                        int qty = Integer.parseInt(qtyStr);
+                        int existingQty = Integer.parseInt(item[3]);
+                        item[3] = Integer.toString(existingQty + qty);
                         break;
                     }
                 }
-
+    
                 if (found) {
                     // Write the updated data back to the file
                     FileWriter writer = new FileWriter(file);
                     for (String[] item : items) {
-                        writer.write(item[0] + "," + item[1] + "," + item[2] + "\n");
+                        writer.write(item[0] + "," + item[1] + "," + item[2] + "," + item[3] + "\n");
                     }
                     writer.close();
-
+    
                     System.out.println("Quantity updated successfully!");
                 } else {
                     System.out.println("Item not found!");
                 }
             } catch (IOException e) {
                 System.out.println("An error occurred: " + e.getMessage());
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid quantity entered. Please enter a valid integer.");
             }
         }
-
-
-
+    }
+    public void checkOutPro() {
+        int index = isWorker();
+        if (index != -1) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter registration number of the item to update:");
+            String regNum = input.nextLine();
+    
+            try {
+                // Open the file for reading
+                File file = new File( "C:\\Users\\suve5\\Downloads\\StockPro Demo\\inventory.txt");
+                Scanner scanner = new Scanner(file);
+    
+                // Create an ArrayList to store the items
+                ArrayList<String[]> items = new ArrayList<>();
+    
+                // Read the data from the file into the ArrayList
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    String[] tokens = line.split(",");
+                    items.add(tokens);
+                }
+                scanner.close();
+    
+                // Find the item with the matching registration number and update the quantity
+                boolean found = false;
+                for (String[] item : items) {
+                    if (item[0].equals(regNum)) {
+                        found = true;
+                        System.out.println("Enter quantity to checkout for " + item[1] + ":");
+                        String qtyStr = input.nextLine();
+                        int qty = 0;
+                        try {
+                            qty = Integer.parseInt(qtyStr);
+                            if (qty < 0) {
+                                throw new IllegalArgumentException("Quantity cannot be negative");
+                            }
+                        } catch (NumberFormatException e) {
+                            throw new IllegalArgumentException("Invalid quantity: " + qtyStr);
+                        }
+                        int currentQty = Integer.parseInt(item[3]);
+                        if (qty > currentQty) {
+                            System.out.println("Insufficient quantity");
+                        } else {
+                            item[3] = Integer.toString(currentQty - qty);
+                            // Write the updated data back to the file
+                            FileWriter writer = new FileWriter(file);
+                            for (String[] it : items) {
+                                writer.write(it[0] + "," + it[1] + "," + it[2] + "," + it[3] + "\n");
+                            }
+                            writer.close();
+    
+                            System.out.println("Checkout successful");
+                        }
+                        break;
+                    }
+                }
+    
+                if (!found) {
+                    System.out.println("Item not found!");
+                }
+            } catch (IOException e) {
+                System.out.println("An error occurred: " + e.getMessage());
+            }
+        }
+    }
+     
+    
+    
     }
 
-
-
+    
+    
