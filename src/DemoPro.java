018f9f32e3d808b4634ead244bcd8a6bd76fea16
Jawad Fahim <72640742+JawadFahim@users.noreply.github.com>
2023-05-02 10:51:15 +0600

    Add files via upload

---
+++ src/DemoPro.java
@@ -0,0 +1,8 @@
+public interface DemoPro {
+    public void dispMenu();
+    public void searchBySno();
+    public void searchByCompanyName();
+    public void searchByProductName();
+    public void addProduct(Productset p);
+    
+}
---
+++ src/DemoWork.java
@@ -0,0 +1,6 @@
+public interface DemoWork {
+
+    public void addWorker(Worker s);
+    public void showAllWorkers();
+    public void checkOutPro();
+}
